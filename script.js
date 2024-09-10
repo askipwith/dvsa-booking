675e19a8065f5fca7f782a6f753d0a07b0a06c5d
Andy Skipwith <andrew.skipwith@me.com>
2024-09-10 13:06:35 +0100

    Update dvsaConfigParameters in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Find new local test
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  try to take over the world!
 // @author       askipwith
 // @match        https://driverpracticaltest.dvsa.gov.uk/*
